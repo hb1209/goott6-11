81d0726f12e17eb913ebc0b6922450f11817df3b
goott6-11 <goott6-11@goott6-11-PC>
2018-12-20 12:40:49 +0900

    수정_충돌해결

--- git-demo-heebok/src/TestMain.java
+++ git-demo-heebok/src/TestMain.java
@@ -3,9 +3,6 @@ public class TestMain {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, Git!!(from goott6-11)");
-		System.out.println("Hello, G~~~~~~~~");
-		System.out.println("Hello, update");
-		System.out.println("Hello, 22222");
 		
 	}
 
