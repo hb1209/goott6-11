18e467a45cc9a997d39a2aab34f3d0dd7dedf95c
goott6-11 <goott6-11@goott6-11-PC>
2018-12-20 12:04:21 +0900

    updated

--- git-demo-heebok/src/TestMain.java
+++ git-demo-heebok/src/TestMain.java
@@ -2,8 +2,8 @@
 public class TestMain {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, Git!!!!!(goott6-11)");
-
+		System.out.println("Hello, Git!!(from HN)");
+		System.out.println("Hello, G~~~~~~~~");
 	}
 
 }
