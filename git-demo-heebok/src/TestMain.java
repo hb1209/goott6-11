f025ac9c99b860bdead2b0207cfd27f0de1579e1
jeonjisu <jeonjisu@192.168.6.152>
2018-12-20 13:33:05 +0900

    hhhhhhh

--- git-demo-heebok/src/TestMain.java
+++ git-demo-heebok/src/TestMain.java
@@ -4,15 +4,12 @@ public class TestMain {
 	public static void main(String[] args) {
 
 
-		System.out.println("Hello, Git!!(from goott6-11)");
-		System.out.println("Hello, Git!!(");
-<<<<<<< HEAD
-		System.out.println("Hello, Git!!!!!(goott6-11)");
-		System.out.println("ohyeah");
+		System.out.println("Hello, Git!!(goott6-11)");
+		System.out.println("Hello, Git!!");
 		System.out.println("Hello, Git!!(from HN)");
 		System.out.println("Hello, G~~~~~~~~");
 		System.out.println("ohyeah");
-=======
+
 
 		System.out.println("Hello, Git!!!!!(goott6-11)");
 
@@ -23,7 +20,7 @@ public class TestMain {
 
 		System.out.println("Hello, Git!!(from HN)");
 		System.out.println("Hello, G~~~~~~~~");
->>>>>>> branch 'master' of https://github.com/hb1209/goott6-11.git
+
 
 	}
 
