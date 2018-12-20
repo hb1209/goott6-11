47eb1a51ac65f2146474890c2b05b6b80b7a117b
jeonjisu <jeonjisu@192.168.6.152>
2018-12-20 12:17:01 +0900

    수수수수수수

--- git-demo-hn/src/TestMain.java
+++ git-demo-hn/src/TestMain.java
@@ -2,13 +2,13 @@
 public class TestMain {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("Hello, Git!!(from HN)");
-		
+
 		System.out.println("Hello, Git~~~~~~~~^^");
-		
-		//t수정했습니다
-	System.out.println("수정다");
+
+		// t수정했습니다
+		System.out.println("수정다");
 
 	}
 
