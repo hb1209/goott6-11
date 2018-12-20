88d5bd8045476fa4663a59166dfcb8b95a396c2b
goott6-11 <goott6-11@goott6-11-PC>
2018-12-20 12:10:32 +0900

    다시 수정~~

--- git-demo-heebok/src/TestMain.java
+++ git-demo-heebok/src/TestMain.java
@@ -4,6 +4,8 @@ public class TestMain {
 	public static void main(String[] args) {
 		System.out.println("Hello, Git!!(from HN)");
 		System.out.println("Hello, G~~~~~~~~");
+		
+		System.out.println("Helloooooooooooooooooo");
 	}
 
 }
